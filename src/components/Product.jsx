4627f0707a8e2fd6a93aa94d1792b2863eba48e0
Mathias Podesta <44898573+mapodesta@users.noreply.github.com>
2022-06-23 11:13:14 +0100

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,7 +4,7 @@ export default function Product({ product, handleAddToCart }) {
   return (
     <div className="Products-item">
       <img src={product.image} alt={product.title} />
-      <div className="Product-item-info">
+      <div className="Products-item-info">
         <h2>
           {product.title}
           <span>${product.price}</span>
